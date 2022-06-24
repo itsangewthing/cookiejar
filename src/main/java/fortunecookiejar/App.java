73e1e84f80c7847bb3643c4afb4007258464044d
partytaitai <partytaitai@MacBook-Pro-3.local>
2022-06-24 15:58:41 +0800

    cookiecommit

---
+++ src/main/java/fortunecookiejar/App.java
@@ -0,0 +1,13 @@
+package fortunecookiejar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
